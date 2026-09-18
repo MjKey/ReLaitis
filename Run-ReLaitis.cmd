fe8ebcbf01801d2010171b2ec0320719ff7027e2
MjKey <go@mjkey.ru>
2026-09-18 22:41:13 +0300

    Update Run-ReLaitis.cmd to prefer Release build

--- Run-ReLaitis.cmd
+++ Run-ReLaitis.cmd
@@ -1,2 +1,6 @@
 @echo off
-start "" "%~dp0src\ReLaitis.UI\bin\Debug\net9.0-windows\ReLaitis.UI.exe"
+if exist "%~dp0src\ReLaitis.UI\bin\Release\net9.0-windows\ReLaitis.UI.exe" (
+    start "" "%~dp0src\ReLaitis.UI\bin\Release\net9.0-windows\ReLaitis.UI.exe"
+) else (
+    start "" "%~dp0src\ReLaitis.UI\bin\Debug\net9.0-windows\ReLaitis.UI.exe"
+)
